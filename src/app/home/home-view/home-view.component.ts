c885486c39dc96cea75f3d09cde836065fa1b21f
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-19 21:44:00 -0400

    Background homeview set

--- src/app/home/home-view/home-view.component.ts
+++ src/app/home/home-view/home-view.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewEncapsulation,
+  Inject,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
   styleUrls: ['./home-view.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class HomeViewComponent implements OnInit {
-  constructor() {}
+export class HomeViewComponent implements OnInit, OnDestroy {
+  constructor(@Inject(DOCUMENT) private _document: Document) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._document.body.classList.add('bodybg-image');
+  }
+  ngOnDestroy() {
+    // remove the class form body tag
+    this._document.body.classList.remove('bodybg-image');
+  }
 }
